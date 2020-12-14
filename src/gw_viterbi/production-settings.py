b714be8077f2182b46a8636020afd4a8c5f66e63
Lewis Lakerink <llakerink@swin.edu.au>
2020-12-14 11:58:57 +1100

    Add missing database search URL to production settings
    Summary: For some reason we forgot to add the database search url to the production settings, which meant that the viterbi public job list was always empty
    
    Test Plan: Works as expected in production. This is hard to test locally.
    
    Reviewers: mr-asher, jesmigel
    
    Reviewed By: jesmigel
    
    Differential Revision: https://phab.adacs.org.au/D373

--- src/gw_viterbi/production-settings.py
+++ src/gw_viterbi/production-settings.py
@@ -13,6 +13,7 @@ EMAIL_PORT = 25
 
 GWCLOUD_JOB_CONTROLLER_API_URL = "http://gwcloud-job-server:8000/job/apiv1"
 GWCLOUD_AUTH_API_URL = "http://gwcloud-auth:8000/auth/graphql"
+GWCLOUD_DB_SEARCH_API_URL = "http://gwcloud-db-search:8000/graphql"
 
 try:
     from .environment import *
