6ceb319362a3cb29540fd17f49f7c2e8c0c4ac52
TomReichardt <thomas.reichardt@students.mq.edu.au>
2022-03-09 15:43:25 +1100

    Secondary menu work, updating test

---
+++ src/react/src/Components/Menu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Link} from 'found';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+const subMenu = () => <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+        <Link className="nav-link" to='/viterbi' exact className="nav-link">
+            Experiments
+        </Link>
+        <Link className="nav-link" to='/viterbi/job-form' exact className="nav-link">
+            New Experiment
+        </Link>
+    </Nav>
+</Navbar.Collapse>;
+
+const Menu = ({name}) => {
+    const SubMenu = subMenu(name);
+    return (
+        <Navbar collapseOnSelect expand="md" fixed="top" className="navbar-secondary">
+            <Container>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                {SubMenu}
+            </Container>
+        </Navbar>
+    );
+};
+
+// const secondaryMenu = () => 
+
+export default Menu;
