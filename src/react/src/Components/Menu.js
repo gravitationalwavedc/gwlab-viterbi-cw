d99900bb136ef20f2ddd09b174f5d9e1cc31613d
TomReichardt <thomas.reichardt@students.mq.edu.au>
2022-03-09 16:51:54 +1100

    Neatening Menu component

--- src/react/src/Components/Menu.js
+++ src/react/src/Components/Menu.js
@@ -4,29 +4,22 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 
-const subMenu = () => <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-        <Link className="nav-link" to='/viterbi' exact>
-            Experiments
-        </Link>
-        <Link className="nav-link" to='/viterbi/job-form' exact>
-            New Experiment
-        </Link>
-    </Nav>
-</Navbar.Collapse>;
-
-const Menu = ({name}) => {
-    const SubMenu = subMenu(name);
-    return (
-        <Navbar collapseOnSelect expand="md" fixed="top" className="navbar-secondary">
-            <Container>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                {SubMenu}
-            </Container>
-        </Navbar>
-    );
-};
-
-// const secondaryMenu = () => 
+const Menu = () => (
+    <Navbar collapseOnSelect expand="md" fixed="top" className="navbar-secondary">
+        <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link className="nav-link" to='/viterbi' exact>
+                        Experiments
+                    </Link>
+                    <Link className="nav-link" to='/viterbi/job-form' exact>
+                        New Experiment
+                    </Link>
+                </Nav>
+            </Navbar.Collapse>;
+        </Container>
+    </Navbar>
+);
 
 export default Menu;
