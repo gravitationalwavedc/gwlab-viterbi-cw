70f63fe759afdd15c07ca94cd1b9c5e3dead21af
TomReichardt <thomas.reichardt@students.mq.edu.au>
2022-03-10 02:46:17 +0000

    Secondary menu
    Adding secondary menu for viterbi
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwlab-viterbi-cw/-/merge_requests/3

---
+++ src/react/src/Components/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'found';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+const Menu = () => (
+    <Navbar collapseOnSelect expand="md" fixed="top" className="navbar-secondary">
+        <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link className="nav-link" to='/viterbi' exact>
+                        Experiments
+                    </Link>
+                    <Link className="nav-link" to='/viterbi/job-form' exact>
+                        New Experiment
+                    </Link>
+                </Nav>
+            </Navbar.Collapse>;
+        </Container>
+    </Navbar>
+);
+
+export default Menu;
--- src/react/src/index.js
+++ src/react/src/index.js
@@ -1,9 +1,11 @@
 import getRoutes from './Routes';
+import Menu from './Components/Menu';
 
 let harnessApi = null;
 
 function setHarnessApi(api) {
     harnessApi = api;
+    harnessApi.getSecondaryMenu = () => Menu;
 }
 
 export {
