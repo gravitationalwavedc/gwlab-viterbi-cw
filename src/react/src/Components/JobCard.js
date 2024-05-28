3a251f6008903534164ef176bcabff8a1cce7766
TomReichardt <thomas.reichardt@students.mq.edu.au>
2024-05-28 05:31:07 +0000

    Updating job card to be clickable, increasing size of duplicate button
    Made job cards clickable, rather than having to click small view link, increased size of the duplicate link.
    #5
    
    Approved-by: Lewis Lakerink <llakerink@swin.edu.au>
    
    Merged by: TomReichardt <thomas.reichardt@students.mq.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/gwlab-viterbi-cw/-/merge_requests/20

--- src/react/src/Components/JobCard.js
+++ src/react/src/Components/JobCard.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import Link from 'found/Link';
-import { HiDuplicate, HiDocumentText} from 'react-icons/hi';
+import { HiDuplicate } from 'react-icons/hi';
 import OzstarLogo from '../assets/ostar_svg.svg';
 
 const JobCard = ({node, match, router}) =>
     <Card className="gwlab-job-card">
-        <Card.Body>
-            <Card.Title>
-                {node.name} 
-            </Card.Title>
-            <Card.Subtitle>{node.description}</Card.Subtitle>
-            <Card.Text>
-                <OzstarLogo style={{position: 'absolute', top: '1rem', right: '1rem', height: '2.2rem'}} />
-                <span className={`status-${node.jobStatus.name.toLowerCase()}`}>
-                    {node.jobStatus.name}
-                </span>
-                {node.user}
-            </Card.Text>
-        </Card.Body>
+        <Link 
+            to={{pathname: '/viterbi/job-results/' + node.id + '/'}} 
+            exact 
+            match={match} 
+            router={router}
+            className="text-body"
+        >
+            <Card.Body>
+                <Card.Title>
+                    {node.name} 
+                </Card.Title>
+                <Card.Subtitle>{node.description}</Card.Subtitle>
+                <Card.Text>
+                    <OzstarLogo style={{position: 'absolute', top: '1rem', right: '1rem', height: '2.2rem'}} />
+                    <span className={`status-${node.jobStatus.name.toLowerCase()}`}>
+                        {node.jobStatus.name}
+                    </span>
+                    {node.user}
+                </Card.Text>
+            </Card.Body>
+        </Link>
         <Card.Footer className="text-right">
-            <Link 
-                to={{pathname: '/viterbi/job-results/' + node.id + '/'}} 
-                activeClassName="selected" 
-                className="card-action"
-                exact 
-                match={match} 
-                router={router}>
-                <HiDocumentText className="mr-1 mb-1" />
-                    View
-            </Link>
             <Link 
                 to={{
                     pathname: '/viterbi/job-form/duplicate/',
@@ -39,13 +37,16 @@ const JobCard = ({node, match, router}) =>
                 }}
                 activeClassName="selected" 
                 className="card-action"
+                style={{fontSize: '20px'}}
                 exact 
                 match={match} 
-                router={router}>
+                router={router}
+            >
                 <HiDuplicate className="mr-1 mb-1" />
                 Duplicate
             </Link>
         </Card.Footer>
-    </Card>;
+    </Card>
+;
 
 export default JobCard;
